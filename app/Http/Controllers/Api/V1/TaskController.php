5876f83bedac1550c235d65791a390d077fc2b3d
diansopandi <dian.sopandi@nalysa.com>
2024-04-30 15:55:37 +0700

    add task resource to make return response json

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
 
 class TaskController extends Controller
 {
@@ -16,7 +18,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return TaskResource::collection(Task::all());
+        return TaskResource::collection(Task::latest()->get());
     }
 
     /**
@@ -32,17 +34,41 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    // public function show(Task $task)
+    public function show($id)
     {
-        return TaskResource::make($task);
+        // return TaskResource::make($task) 
+        $task = Task::find($id);
+        return $task ? TaskResource::make($task) : response()->json(['data' => null, 'message' => 'Task not found'], 404);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(UpdateTaskRequest $request, $id)
     {
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['error' => 'Task not found'], 404);
+        }
+
         $task->update($request->validated());
+
+        /**
+         * 
+         *try {
+         *    $existTask = Task::findOrFail($task->id);
+         *    $existTask->update($request->validated());
+         *} catch (ModelNotFoundException $e) {
+         *     return response()->json(['data' => null]);
+         *}
+         */
+
+        $validated = json_encode($request->validated());
+        $message = "Task {$validated} updated successfully";
+
+        Log::info($message);
 
         return TaskResource::make($task);
     }
@@ -50,8 +76,10 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
